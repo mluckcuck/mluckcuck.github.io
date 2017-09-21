25bc069cd6126b0f605918916732ee44e640d3d7
Matt Luckcuck <ml881@york.ac.uk>
2017-09-21 11:31:25 +0100

    Added to files

---
+++ files/yorkLearningCourses/ccb/week1/rectangle.py
@@ -0,0 +1,14 @@
+"""Draw a purple rectangle """
+
+from turtle import *
+
+
+length = 100
+width = 60
+
+
+penup()
+setpos(-200,-200)
+pendown()
+
+
---
+++ files/yorkLearningCourses/ccb/week1/square.py
@@ -0,0 +1,13 @@
+"""Draw a blue square """
+
+from turtle import *
+
+
+length = 100
+angle = 90 
+
+penup()
+setpos(200,200)
+pendown()
+
+
